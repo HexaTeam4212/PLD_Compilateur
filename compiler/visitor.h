8e5c0cc8ebdcfa0232b8cb6fd741be6f8bdd1fae
Wade Fatoumata Binetou Rassoul <fbrwade@if501-219-11.insa-lyon.fr>
2020-03-09 12:05:13 +0100

    visiteurs ok pour expr

--- compiler/visitor.h
+++ compiler/visitor.h
@@ -18,10 +18,13 @@ public:
   virtual antlrcpp::Any visitAxiom(ifccParser::AxiomContext *ctx) override {
     return visitChildren(ctx);
   }
+  
+  /*virtual antlrcpp::Any visitAxiom1(ifccParser::Axiom1Context *ctx) override {
+    return visitChildren(ctx);
+  }*/
 
   virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override {
-
-    int retval = stoi(ctx->CONST()->getText());
+    
     std::cout<<".text\n"
     ".globl	main\n"
     " main: \n"
@@ -29,15 +32,61 @@ public:
     "  pushq %rbp # save %rbp on the stack \n"
     "  movq %rsp, %rbp  # define %rbp for the current function \n"
     
-    "# body \n"
-    "  movl	$"<<retval<<", %eax\n"
+    "# body \n";
+    //visit(ctx->bloc());
+    int retval = visit(ctx->expr());
+    std::cout<<"  movl	$"<<retval<<", %eax\n"
     
     "# epilogue \n"
-    "popq %rbp  # restore %rbp from the stack \n"
-    "ret  # return to the caller (here the shell) \n";
+    " popq %rbp  # restore %rbp from the stack \n"
+    " ret  # return to the caller (here the shell) \n";
     
     return 0;
   }
+
+  virtual antlrcpp::Any visitBloc(ifccParser::BlocContext *ctx) override {
+    return visitChildren(ctx);
+  }
+ 
+ virtual antlrcpp::Any visitInstructions(ifccParser::InstructionsContext *ctx) override {
+   return visitChildren(ctx);
+ }
+  
+  
+  virtual antlrcpp::Any visitInstruction(ifccParser::InstructionContext *ctx) override {
+    return visitChildren(ctx);
+  }
+  
+  virtual antlrcpp::Any visitDeclaration(ifccParser::DeclarationContext *ctx) override {
+    
+    
+    
+    
+    return 0;
+  }
+  
+  virtual antlrcpp::Any visitAffectation(ifccParser::AffectationContext *ctx) override {
+   
+    
+    
+    
+    return 0;
+  }
+  
+  /*virtual antlrcpp::Any visitExpr(ifccParser::ExprContext *ctx) override {
+    return visitChildren(ctx);
+  }*/
+  
+  virtual antlrcpp::Any visitVar(ifccParser::VarContext *ctx) override {
+    //Recuperer index dans table des symboles pour avoir l'adresse memoire
+    return 4 ;
+  }
+  
+  virtual antlrcpp::Any visitConst(ifccParser::ConstContext *ctx) override {
+    std::cout<<"visite const"<<std::endl;
+    return(int) stoi (ctx->CONST()->getText());
+  }
+  
 
 
 };
