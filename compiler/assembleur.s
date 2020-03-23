3aa1001f56bbb762cd23bde3d9e7ced5b392ec71
LucieMBovo <lucie.bovo@insa-lyon.fr>
2020-03-23 08:30:50 +0100

    nouvel essai sans .vd

---
+++ compiler/assembleur.s
@@ -0,0 +1,16 @@
+.text
+.global main
+main:
+	pushq %rbp
+	movq %rsp, %rbp
+BB0:
+	movq $42, -16(%rbp)
+	movq -16(%rbp), %rax
+	movq %eax, -8(%rbp)
+	movq $7, -24(%rbp)
+	movq -24(%rbp), %rax
+	movq %eax, -8(%rbp)
+	movq -8(%rbp), %rax
+endmain:
+	popq %rbp
+	ret
